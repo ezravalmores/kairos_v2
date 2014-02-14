1c903e6c278921d1a2410fc09546ed6394793266
Ezra Valmores <evalmores@nazarene.org>
2014-02-15 06:13:09 +0800

    fixed layout

--- app/models/specific_task.rb
+++ app/models/specific_task.rb
@@ -7,6 +7,7 @@ class SpecificTask < ActiveRecord::Base
   belongs_to :organization
   
   validates_presence_of :name, :description, :organization_id
+  validates_uniqueness_of :name
   scope :active, where(:is_active => true)
   
   #instance method
