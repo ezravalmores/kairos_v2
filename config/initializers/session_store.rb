1929576e4f290206cf639b7d98488a47b9f99cb1
Ezra Valmores <evalmores@nazarene.org>
2014-03-05 07:55:03 +0800

    removed files

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Kairos::Application.config.session_store :cookie_store, key: '_kairos_session'
+#Kairos::Application.config.session_store :cookie_store, key: '_kairos_session'
+#Kairos::Application.config.session_store :active_record_store
