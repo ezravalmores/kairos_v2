f16c601d2d1125c5842ab57e0c9ad3fbee4d410b
Ezra Valmores <evalmores@nazarene.org>
2014-03-05 15:30:32 +0800

    approval controlles

---
+++ app/controllers/approvals_controller.rb
@@ -0,0 +1,14 @@
+class ApprovalsController < ApplicationController
+  require 'will_paginate/array'
+ 
+  before_filter :authorize
+  
+  def tasks_approval
+    
+    @for_approvals = PersonTask.find_tasks_to_be_approved([:task,:specific_task,{:person => [:department, :organization] }],current_user)
+    @total_count = @for_approvals.length 
+
+    @for_approvals = @for_approvals.paginate(:page => params[:page],:per_page => 100)
+  end
+  
+end  
