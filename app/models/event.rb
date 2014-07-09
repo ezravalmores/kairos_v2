ccb178ab8a63f79cb1c141de224cecfc07c97a79
Ezra Valmores <evalmores@nazarene.org>
2014-07-10 01:31:26 +0800

    removed tracked

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,5 @@
 class Event < ActiveRecord::Base
   include PublicActivity::Common
-  include PublicActivity::Model
-  tracked
   
   belongs_to :creator, :class_name => "Person", :foreign_key => "created_by"
   
